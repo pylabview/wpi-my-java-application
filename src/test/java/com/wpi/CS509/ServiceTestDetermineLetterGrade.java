d7abd33f2c655b8ded6700932ceb9c4384251d15
Rodrigo Arguello Serrano <rodrigo19662001@gmail.com>
2024-04-29 10:36:53 -0700

    Correcting compatibility issues with JDK 17

--- src/test/java/com/wpi/CS509/ServiceTestDetermineLetterGrade.java
+++ src/test/java/com/wpi/CS509/ServiceTestDetermineLetterGrade.java
@@ -7,41 +7,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class ServiceTestDetermineLetterGrade {
     @Test
     void fiftyNineReturnF(){
-        var service = new Service();
+        Service service = new Service();
         char result = service.determineLetterGrade(59);
         assertEquals('F', result);
     }
     @Test
     void sixtyNineReturnD(){
-        var service = new Service();
+        Service service = new Service();
         char result = service.determineLetterGrade(69);
         assertEquals('D', result);
     }
 
     @Test
     void seventyNineReturnC(){
-        var service = new Service();
+        Service service = new Service();
         char result = service.determineLetterGrade(79);
         assertEquals('C', result);
     }
 
     @Test
     void eightyNineReturnB(){
-        var service = new Service();
+        Service service = new Service();
         char result = service.determineLetterGrade(89);
         assertEquals('B', result);
     }
 
     @Test
     void ninetyNineReturnA(){
-        var service = new Service();
+        Service service = new Service();
         char result = service.determineLetterGrade(99);
         assertEquals('A', result);
     }
 
     @Test
     void negativeOneReturnsIllegalArgumentException(){
-        var service = new Service();
+        Service service = new Service();
         assertThrows(IllegalArgumentException.class,
                 ()->{
             service.determineLetterGrade(-1);
